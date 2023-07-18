5cd6602112fa1a88ead0c3932e4cdcfca6d01301
fatmanurul <fatmanurul555@gmail.com>
2023-07-18 20:43:06 +0700

    membuat view login & registration eps 14

--- App-laravel/resources/views/partials/navbar.blade.php
+++ App-laravel/resources/views/partials/navbar.blade.php
@@ -4,7 +4,7 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="right navbar-right " id="navbarNav">
+    <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
         <a class="nav-link {{ ($active === "home") ? 'active' : '' }}" href="/">Home</a>
@@ -17,6 +17,11 @@
         </li>
         <li class="nav-item">
         <a class="nav-link {{ ($active === "Categories") ? 'active' : '' }}" href="/categories">Categories</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}" ><i class="bi bi-box-arrow-in-right"></i> login</a>
         </li>
       </ul>
     </div>
--- App-laravel/routes/web.php
+++ App-laravel/routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Models\Post;
+// use App\Models\Post;
+// use App\Models\User;
+use App\Models\Category;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
-use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -62,3 +64,7 @@ Route::get('/categories', function(){
 //         'posts' => $author->posts->load('category','author'),
 //     ]);
 // });
+
+
+Route::get('/login', [LoginController::class ,'index']);
+Route::get('/register', [RegisterController::class ,'index']);

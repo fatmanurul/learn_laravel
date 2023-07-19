d9d11e808ec429774d11d67c9992713f5928cd9c
fatmanurul <fatmanurul555@gmail.com>
2023-07-19 18:00:51 +0700

    Dashboard UI eps 17

--- App-laravel/app/Models/Post.php
+++ App-laravel/app/Models/Post.php
@@ -42,4 +42,8 @@ class Post extends Model
     {
         return $this->belongsTo(User::class, 'user_id');   
     }
+    public function getRouteKeyName()
+{
+    return 'slug';
+}
 }
--- App-laravel/resources/views/partials/navbar.blade.php
+++ App-laravel/resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
   <div class="container">
-    <a class="navbar-brand" href="/">Brownies Blog</a>
+    <a class="navbar-brand" href="/">Chocolate Cake Blog</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -19,10 +19,30 @@
         <a class="nav-link {{ ($active === "Categories") ? 'active' : '' }}" href="/categories">Categories</a>
         </li>
       </ul>
+
+      <!-- kalo belum login tampilin login,kalo uda login tampilkan yang lain -->
       <ul class="navbar-nav ms-auto">
+        @auth 
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Welcome Back, {{ auth()->user()->name }}
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li>
+              <form action="/logout" method="post">
+                @csrf
+                <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</button>
+              </form>
+            </li>
+          </ul>
+        </li>
+        @else
         <li class="nav-item">
           <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}" ><i class="bi bi-box-arrow-in-right"></i> login</a>
         </li>
+        @endauth
       </ul>
     </div>
   </div>
